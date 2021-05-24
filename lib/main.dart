6d1a4f8618557a5bd67b8a4ad60efcc9b4e11da3
arshiacerd <62174601+arshiacerd@users.noreply.github.com>
2021-05-24 16:42:31 +0500

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text("Aqsa")),
-    );
-  }
+        //Container
+        // body: Container(child: Text("Container child"),
+        // height: 200,
+        // width: 200,
+        // color: Colors.blueGrey,
+        
+        // ),
+
+      //  Coloumn
+        body:SingleChildScrollView(
+          child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,      
+          mainAxisAlignment: MainAxisAlignment.center,
+         
+          children: [
+         Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+             SizedBox(height:20),
+              Container(
+           height:100,
+           width:100,
+           color: Colors.brown,
+             ),
+            
+              ],
+        
+        
+        ),)
+        //row
+        // body: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        //   children:[
+        //     Text("1st row"),
+        //     Text("2nd row"),
+        //     //SizedBox(width:30),
+        //     Text("r row"),
+
+        //   ]
+        // ),
+    
+        ),
+    ); 
+     }
 }
